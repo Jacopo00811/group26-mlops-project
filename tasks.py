70faf1e6f0d72149f5feac10760e1421b50020ae
MrMustii <mustii.elmadani@gmail.com>
2025-01-24 17:13:13 +0100

    tasks done

--- tasks.py
+++ tasks.py
@@ -12,7 +12,7 @@ PYTHON_VERSION = "3.11"
 def create_environment(ctx: Context) -> None:
     """Create a new conda environment for project."""
     ctx.run(
-        f"conda create --name {PROJECT_NAME} python={PYTHON_VERSION} pip --no-default-packages --yes",
+        f"uv venv {PROJECT_NAME} python={PYTHON_VERSION} pip --no-default-packages --yes",
         echo=True,
         pty=not WINDOWS,
     )
@@ -21,7 +21,6 @@ def create_environment(ctx: Context) -> None:
 @task
 def requirements(ctx: Context) -> None:
     """Install project requirements."""
-    ctx.run("pip install -U pip setuptools wheel", echo=True, pty=not WINDOWS)
     ctx.run("pip install -r requirements.txt", echo=True, pty=not WINDOWS)
     ctx.run("pip install -e .", echo=True, pty=not WINDOWS)
 
@@ -32,23 +31,26 @@ def dev_requirements(ctx: Context) -> None:
     ctx.run('pip install -e .["dev"]', echo=True, pty=not WINDOWS)
 
 
-# Project commands
-@task
-def preprocess_data(ctx: Context) -> None:
-    """Preprocess data."""
-    ctx.run(f"python src/{PROJECT_NAME}/data.py data/raw data/processed", echo=True, pty=not WINDOWS)
+@task(requirements)
+def api_requirements(ctx: Context) -> None:
+    ctx.run("pip install -r requirements_frontend.txt", echo=True, pty=not WINDOWS)
+    ctx.run("pip install -r requirements_backend.txt", echo=True, pty=not WINDOWS)
 
 
 @task
-def train(ctx: Context) -> None:
+def train(ctx: Context, config_path: str = "../../configs", config_name: str = "config.yaml") -> None:
     """Train model."""
-    ctx.run(f"python src/{PROJECT_NAME}/train.py", echo=True, pty=not WINDOWS)
+    ctx.run(
+        f"python src/{PROJECT_NAME}/train.py --config-path {config_path} --config-name {config_name}",
+        echo=True,
+        pty=not WINDOWS,
+    )
 
 
 @task
 def test(ctx: Context) -> None:
     """Run tests."""
-    ctx.run("coverage run -m pytest tests/", echo=True, pty=not WINDOWS)
+    ctx.run("coverage run -m pytest tests/unit_tests", echo=True, pty=not WINDOWS)
     ctx.run("coverage report -m", echo=True, pty=not WINDOWS)
 
 
@@ -63,16 +65,21 @@ def docker_build(ctx: Context, progress: str = "plain") -> None:
     ctx.run(
         f"docker build -t api:latest . -f dockerfiles/api.dockerfile --progress={progress}", echo=True, pty=not WINDOWS
     )
+    ctx.run(
+        f"docker build -t front_api:latest . -f dockerfiles/front_api.dockerfile --progress={progress}",
+        echo=True,
+        pty=not WINDOWS,
+    )
 
 
 # Documentation commands
 @task(dev_requirements)
 def build_docs(ctx: Context) -> None:
     """Build documentation."""
-    ctx.run("mkdocs build --config-file docs/mkdocs.yaml --site-dir build", echo=True, pty=not WINDOWS)
+    ctx.run("mkdocs build --config-file mkdocs.yml --site-dir build", echo=True, pty=not WINDOWS)
 
 
 @task(dev_requirements)
 def serve_docs(ctx: Context) -> None:
     """Serve documentation."""
-    ctx.run("mkdocs serve --config-file docs/mkdocs.yaml", echo=True, pty=not WINDOWS)
+    ctx.run("mkdocs serve --config-file mkdocs.yml", echo=True, pty=not WINDOWS)
